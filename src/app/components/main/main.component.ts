3a35a84d778553729ecae6466ca158ae2d18f380
shawnmarkus <shawnmarkus4200@gmail.com>
2023-06-17 07:48:54 +0530

    task are adding up

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/services/crud.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'],
+})
+export class MainComponent implements OnInit {
+  taskObj: Task = new Task();
+  taskArr: Task[] = [];
+
+  addTaskValue: string = '';
+  editTaskValue: string = '';
+
+  constructor(private crudService: CrudService) {}
+
+  ngOnInit(): void {
+    this.editTaskValue = '';
+    this.addTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(
+      (res) => {
+        this.taskArr = res;
+      },
+      (err) => {
+        alert('Unable to get list of tasks');
+      }
+    );
+  }
+
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => {
+        alert(e);
+      },
+      complete: () => {
+        this.ngOnInit();
+        this.addTaskValue = '';
+      },
+    });
+  }
+
+  editTask() {
+    this.taskObj.task_name = this.editTaskValue;
+    this.crudService.editTask(this.taskObj).subscribe({
+      complete: () => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        alert('Failed to update task ' + err);
+      },
+    });
+  }
+
+  deleteTask(etask: Task) {
+    this.crudService.deleteTask(etask).subscribe({
+      complete: () => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        alert('Failed to delete task ' + err);
+      },
+    });
+  }
+
+  call(etask: Task) {
+    this.taskObj = etask;
+    this.editTaskValue = etask.task_name;
+  }
+}
